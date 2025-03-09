4673824136e2796426f501b741ed1df2650ed01e
Bernardo López <bernardolopezf.dev@gmail.com>
2025-03-09 17:23:42 -0500

    Update:index.html Add: carousel on script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
-const carrusel = document.querySelector('.relative');
-const img = carrusel.querySelectorAll('img');
-let indiceActual = 0;
-
-setInterval(() => {
-    imagenes[indiceActual].classList.remove('block');
-    imagenes[indiceActual].classList.add('hidden');
-    indiceActual = (indiceActual + 1) % imagenes.length;
-    imagenes[indiceActual].classList.remove('hidden');
-    imagenes[indiceActual].classList.add('block');
-  }, 1000); // tiempo de transición entre imágenes
+document.addEventListener('DOMContentLoaded', () => {
+  const carousel = document.getElementById('carousel');
+  const prevButton = document.getElementById('prev-button');
+  const nextButton = document.getElementById('next-button');
+  const images = document.querySelectorAll('#carousel img');
+  
+  let currentIndex = 2;
+  
+  prevButton.addEventListener('click', () => {
+    currentIndex--;
+    if (currentIndex < 0) {
+      currentIndex = images.length - 1;
+    }
+    updateCarousel();
+  });
+  
+  nextButton.addEventListener('click', () => {
+    currentIndex++;
+    if (currentIndex >= images.length) {
+      currentIndex = 0;
+    }
+    updateCarousel();
+  });
+  
+  function updateCarousel() {
+    images.forEach((image, index) => {
+      if (index === currentIndex) {
+        image.classList.add('scale-100');
+        image.classList.remove('scale-50');
+      } else {
+        image.classList.remove('scale-100');
+        image.classList.add('scale-50');
+      }
+    });
+  }  
+});
